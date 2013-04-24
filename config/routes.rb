78511cebaf39e06db83c4befde1cd91273b50f4e
Jack <jackm321@gmail.com>
2013-04-24 10:59:30 -0400

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ KcwWorld::Application.routes.draw do
   resources :categories
   resources :proverbs
   resources :photos
+  resources :articles
     
   match 'home' => 'home#index', :as => :home
   root :to => 'home#index'
+
 
 end
